60b2192b7560bccee887c99404be235828ddb8dd
wangyibing <768334398@qq.com>
2021-04-02 22:32:39 +0800

    aaaa

--- data-center-plus/data-center-web/src/main/java/com/softium/datacenter/paas/web/service/JobServiceProxy.java
+++ data-center-plus/data-center-web/src/main/java/com/softium/datacenter/paas/web/service/JobServiceProxy.java
@@ -1,9 +1,4 @@
 package com.softium.datacenter.paas.web.service;
 
 import com.softium.datacenter.paas.api.dto.JobTaskDTO;
-
-public interface JobServiceProxy {
-    void updateJobLog(String jobLogId,String status,String message);
-    //新增定时任务执行记录入库
-    void addJobTask(JobTaskDTO taskDTO);
-}
+666666666666666666
